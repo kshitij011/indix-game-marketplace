d22173409adb035ebbc3717e559e3adf8715a4d1
Kshitij Bhoite <kshitijbhoite011@gmail.com>
2026-01-13 17:14:35 +0530

    improved Indix.sol

--- contracts/Indix.sol
+++ contracts/Indix.sol
@@ -9,36 +9,44 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 contract Indix is Initializable, PausableUpgradeable, OwnableUpgradeable, ERC1155URIStorageUpgradeable {
 
     uint256 public GAME_ID;
-    uint256 gameIdCap;
-    uint256 totalTokens = 1;
+    uint256 totalGameKeys;
+
+    // Token Id 1 reserved for gameKey, rest tokens for skins are incremented.
+    uint256 totalSkinTypes = 1;
 
     uint256 public gameKeyPrice;
     address public GAME_DEVELOPER;
 
     struct Skin{
         string name;
-        uint256 availableQuantity;
-        address owner;
         uint256 price;
     }
 
-    struct ListForRent {
-        uint256 quantity;
-        uint256 price;
+    struct RentListing {
+        uint256 pricePerUnit;
+        uint256 duration;      // seconds
+        uint256 available;     // how many units owner is renting out
+    }
+
+    struct ActiveRent {
+        address owner;
+        uint256 amount;
+        uint256 expiresAt;
     }
 
     // nested mapping incase if we want to list skins for sale or else we will need to create other mapping to get the owner.
     mapping(uint256 => mapping(address => Skin)) public skins;
 
-    mapping(address => mapping(uint256 => ListForRent)) public listedForRent;
-    // mapping(uint256 => Skin) public skins;
-    mapping(address => uint256) public rentDeadline;
+    mapping(uint256 => mapping(address => RentListing)) public rentListings;
+    mapping(uint256 => mapping(address => ActiveRent)) public activeRents;
 
-    // checks if a SkinId is rented/lended from _from address to _to address
-    // mapping(uint256 => mapping(address => mapping(address => bool))) rentedTo;
-    mapping(uint256 => mapping(address => mapping(address => bool))) lendedTo;
-    mapping(address => uint256) rentedAmount;
-    mapping(uint256 => mapping(address => bool)) rented;
+    modifier onlyOwnerOrRenter(uint256 skinId) {
+        if (balanceOf(msg.sender, skinId) > 0) return;
+
+        ActiveRent memory rent = activeRents[skinId][msg.sender];
+        require(rent.expiresAt > block.timestamp, "No usage rights");
+        _;
+    }
 
     function initialize(uint256 _gameId, address _gameDeveloper, uint256 _keyPrice)external initializer {
         GAME_ID = _gameId;
@@ -55,40 +63,35 @@ contract Indix is Initializable, PausableUpgradeable, OwnableUpgradeable, ERC115
         _setURI(newuri);
     }
 
+    // ---CORE LOGIC---
     function purchaseGameKey() public whenNotPaused payable {
         require(msg.value == gameKeyPrice, "Invalid price!");
         require(msg.sender != address(0), "Invalid account");
-        require(gameIdCap > 0, "GameId's exhausted!");
+        require(totalGameKeys > 0, "Game keys exhausted!");
 
         _mint(msg.sender, 1, 1, "");
-        gameIdCap--;
+        totalGameKeys--;
     }
 
     function createSkin(string memory _name, uint256 _availableQuantity, uint256 _price, string memory _uri) public whenNotPaused returns(uint256){
         require(msg.sender == GAME_DEVELOPER, "You cannot create skin");
 
-        totalTokens++;
-        skins[totalTokens][GAME_DEVELOPER] = Skin(_name, _availableQuantity, GAME_DEVELOPER, _price);
-        // skins[totalTokens] = Skin(_name, _availableQuantity, GAME_DEVELOPER, _price);
-        // _mint(GAME_DEVELOPER, totalTokens, _availableQuantity, "");
-        safeMint(GAME_DEVELOPER, totalTokens, _availableQuantity, _uri);
-        return totalTokens;
+        totalSkinTypes++;
+        skins[totalSkinTypes][GAME_DEVELOPER] = Skin(_name, _price);
+        safeMint(GAME_DEVELOPER, totalSkinTypes, _availableQuantity, _uri);
+        return totalSkinTypes;
     }
 
-    function purchaseSkin(uint256 _skinId, address _to) public payable {
+    function purchaseSkinFor(uint256 _skinId, address _to) public payable {
+        require(_skinId > 1, "Can only purchase skins id's >= 2");
+        require(_to != address(0), "Invalid recipient");
         require(msg.value == skins[_skinId][GAME_DEVELOPER].price, "Invalid skin price");
-        // require(msg.value == skins[_skinId].price);
-        require(skins[_skinId][GAME_DEVELOPER].availableQuantity > 1, "Token doesn't exist!");
-        // require(skins[_skinId].availableQuantity > 1, "Skins exhausted!");
-        require(_skinId > 1, "Cannot purchase");
+        require(balanceOf(GAME_DEVELOPER, _skinId) > 0, "Token doesn't exist or out of stock!");
 
-        // skins[_skinId][GAME_DEVELOPER].availableQuantity--;
-        skins[_skinId][GAME_DEVELOPER].availableQuantity--;
+        skins[_skinId][_to].price = msg.value;
+        safeTransferFrom(GAME_DEVELOPER, _to, _skinId, 1, "");
 
-        uint256 buyerBalance = skins[_skinId][_to].availableQuantity++;
-        skins[_skinId][_to] = Skin(skins[_skinId][GAME_DEVELOPER].name, buyerBalance, _to, 0);
-        safeTransferFrom(skins[_skinId][GAME_DEVELOPER].owner, _to, _skinId, 1, "");
-
+        // platform fee is 1 %
         uint256 platformFee = (msg.value * 1) / 100;
         uint256 developerProfit = msg.value - platformFee;
 
@@ -97,106 +100,82 @@ contract Indix is Initializable, PausableUpgradeable, OwnableUpgradeable, ERC115
 
     }
 
-    function increaseGameIdCap(uint256 _amount) public whenNotPaused {
+    function increaseTotalGameKeys(uint256 _amount) public whenNotPaused {
         require(msg.sender == GAME_DEVELOPER, "You cannot increase keys cap");
-        gameIdCap += _amount;
+        require(_amount > 0, "amount 0");
+        totalGameKeys += _amount;
     }
 
     function quantityAvailable(uint256 _skinId, address _address) public view returns(uint256){
         require(_skinId > 1, "TokenId not skin!");
-        return skins[_skinId][_address].availableQuantity;
+        return balanceOf(_address, _skinId);
     }
 
-    // function rentSkin(uint256 _skinId, address _skinOwner) public payable {
-    //     require(msg.value == 0.001 ether);
-    //     require(!rentedTo[_skinId][GAME_DEVELOPER][msg.sender], "Skin already rented");
-    //     require(!lendedTo[_skinId][GAME_DEVELOPER][msg.sender], "Skin already lended");
+    // ---RENTING PART---
+    function listForRent(
+        uint256 skinId,
+        uint256 quantity,
+        uint256 pricePerUnit,
+        uint256 duration
+    ) external {
+        require(balanceOf(msg.sender, skinId) >= quantity, "Insufficient balance");
+        require(quantity > 0 && pricePerUnit > 0 && duration > 0, "Invalid params");
 
-    //     uint256 platformFee = (msg.value * 2) / 100;
-    //     uint256 developerRoyalty = (msg.value * 3) / 100;
-    //     uint256 skinOwnerProfit = msg.value - (platformFee + developerRoyalty);
-
-    //     (bool success, ) = GAME_DEVELOPER.call{value: developerRoyalty}("");
-    //     require(success, "Transfer failed");
-
-    //     (bool success2, ) = skins[_skinId][_skinOwner].owner.call{value: skinOwnerProfit}("");
-    //     require(success2, "Transfer failed");
-
-    //     rentDeadline[msg.sender] = block.timestamp + 1 weeks;
-    //     // safeTransferFrom(skins[_skinId][GAME_DEVELOPER].owner, msg.sender, _skinId, 1, "");
-    //     rentedTo[_skinId][GAME_DEVELOPER][msg.sender] = true;
-    // }
-
-    function unlistFromRent(uint256 _skinId, uint _quantity) external {
-        require(listedForRent[msg.sender][_skinId].quantity > 0, "No NFT's to unlist!");
-        listedForRent[msg.sender][_skinId].quantity -= _quantity;
+        rentListings[skinId][msg.sender] = RentListing({
+            pricePerUnit: pricePerUnit,
+            duration: duration,
+            available: quantity
+        });
     }
 
-    function listForRent(uint256 _skinId, uint256 _quantity, uint256 _price) external {
-        require(balanceOf(msg.sender, _skinId) > 1, "Not enough balance");
-        require(listedForRent[msg.sender][_skinId].quantity > 0, "No NFT's to unlist!");
-        listedForRent[msg.sender][_skinId] = ListForRent(_quantity, _price);
+    function unlistFromRent(uint256 skinId) external {
+        delete rentListings[skinId][msg.sender];
     }
 
-    function rentSkin(uint256 _skinId, address _skinOwner) public payable {
-        require(msg.value == listedForRent[_skinOwner][_skinId].price, "Pay listed price");
+    function rentSkin(
+        uint256 skinId,
+        address owner,
+        uint256 amount
+    ) external payable {
+        RentListing storage listing = rentListings[skinId][owner];
 
-        rented[_skinId][msg.sender] = true;
+        require(listing.available >= amount, "Not enough listed");
+        require(balanceOf(owner, skinId) >= amount, "Owner balance changed");
 
+        uint256 totalPrice = listing.pricePerUnit * amount;
+        require(msg.value == totalPrice, "Incorrect payment");
+
+        // fees
         uint256 platformFee = (msg.value * 2) / 100;
         uint256 developerRoyalty = (msg.value * 3) / 100;
-        uint256 skinOwnerProfit = msg.value - (platformFee + developerRoyalty);
+        uint256 ownerProfit = msg.value - platformFee - developerRoyalty;
 
-        (bool success, ) = GAME_DEVELOPER.call{value: developerRoyalty}("");
-        require(success, "Transfer failed");
+        payable(GAME_DEVELOPER).transfer(developerRoyalty);
+        payable(owner).transfer(ownerProfit);
 
-        (bool success2, ) = skins[_skinId][_skinOwner].owner.call{value: skinOwnerProfit}("");
-        require(success2, "Transfer failed");
+        listing.available -= amount;
 
-        rentDeadline[msg.sender] = block.timestamp + 1 weeks;
-        safeTransferFrom(_skinOwner, msg.sender, _skinId, 1, "");
-        // rentedTo[_skinId][GAME_DEVELOPER][msg.sender] = true;
+        activeRents[skinId][msg.sender] = ActiveRent({
+            owner: owner,
+            amount: amount,
+            expiresAt: block.timestamp + listing.duration
+        });
     }
 
-    function rentTimeleft() public view returns(uint256){
-        uint256 deadline = rentDeadline[msg.sender];
-        uint256 timeLeft = deadline - block.timestamp;
-        return timeLeft;
+    // Check rental validity
+    function isRented(uint256 skinId, address user) public view returns (bool) {
+        return activeRents[skinId][user].expiresAt > block.timestamp;
     }
 
-    // function pullRentedSkin(address _from, uint _skinId) public{
-    //     require(rentedTo[_skinId][GAME_DEVELOPER][msg.sender], "Skin not rented!");
-    //     require(rentTimeleft() == 0, "Time's left to pull");
-    //     require(skins[_skinId][msg.sender].owner == msg.sender, "You're not owner!");
+    // Clearing epired rentals
+    function clearExpiredRent(uint256 skinId) external {
+        ActiveRent storage rent = activeRents[skinId][msg.sender];
+        require(rent.expiresAt <= block.timestamp, "Still active");
 
-    //     rentedTo[_skinId][GAME_DEVELOPER][msg.sender] = true;
-    //     safeTransferFrom(_from, msg.sender, _skinId, 1, "");
-    // }
-
-    function pullRentedSkin(address _from, uint _skinId) public {
-        require(rented[_skinId][_from], "Not rented");
-        // require(rentedTo[_skinId][GAME_DEVELOPER][msg.sender], "Skin not rented!");
-        require(rentTimeleft() == 0, "Time's left to pull");
-        require(skins[_skinId][msg.sender].owner == msg.sender, "You're not owner!");
-
-        // rentedTo[_skinId][GAME_DEVELOPER][msg.sender] = true;
-        safeTransferFrom(_from, msg.sender, _skinId, 1, "");
+        delete activeRents[skinId][msg.sender];
     }
 
-    // lend without any funds
-    // function lendSkin(uint256 _skinId) public {
-    //     // require(msg.value == 0.001 ether);
-    //     require(!rentedTo[_skinId][GAME_DEVELOPER][msg.sender], "Skin already rented");
-    //     // rentDeadline[msg.sender] = block.timestamp + 1 weeks;
-    //     // safeTransferFrom(skins[_skinId][GAME_DEVELOPER].owner, msg.sender, _skinId, 1, "");
-    //     lendedTo[_skinId][GAME_DEVELOPER][msg.sender] = true;
-    // }
-
-    // function pullLendedSkin(uint256 _skinId) public {
-    //     require(lendedTo[_skinId][GAME_DEVELOPER][msg.sender], "Skin not lended");
-    //     lendedTo[_skinId][GAME_DEVELOPER][msg.sender] = false;
-    // }
-
+    // ---OTHER FUNCTIONS---
     // only owner (IndixFactory can pause / unpause)
     function pause() external onlyOwner {
         _pause();
@@ -223,13 +202,23 @@ contract Indix is Initializable, PausableUpgradeable, OwnableUpgradeable, ERC115
         _setURI(_skinId, _uri);
     }
 
+    // users cannot set approvals to other users, only those who have purchased it has the control.
     function setApprovalForAll(address operator, bool approved) public virtual override {
         require(false,"Cannot set approval");
         _setApprovalForAll(_msgSender(), operator, approved);
     }
 
     function safeTransferFrom(address from, address to, uint256 id, uint256 value, bytes memory data) public virtual override{
-        require(rented[id][msg.sender], "NFT is rented cannot transfer");
+        ActiveRent memory rent = activeRents[id][from];
+
+        uint256 locked = 0;
+
+        if(rent.expiresAt > block.timestamp){
+            locked = rent.amount;
+        }
+
+        require(balanceOf(from, id) - locked >= value,"Insufficient transferable balance");
+
         address sender = _msgSender();
         if (from != sender && !isApprovedForAll(from, sender)) {
             revert ERC1155MissingApprovalForAll(sender, from);
@@ -237,6 +226,7 @@ contract Indix is Initializable, PausableUpgradeable, OwnableUpgradeable, ERC115
         _safeTransferFrom(from, to, id, value, data);
     }
 
+    // users cannot transfer the skins to other users.
     function safeBatchTransferFrom(
         address from,
         address to,
